d4a07483e32e533e9db0bd4fbcddd11fd25df9bd
mbedded <github@mbuege.com>
2020-09-27 10:18:32 +0200

    Move http-methods to new HttpRequestHandler.go

--- Main.go
+++ Main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"./models"
 	"./services"
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -17,21 +13,22 @@ import (
 var _templates map[string]*template.Template
 var _router = mux.NewRouter()
 var _dbManager = services.NewDatabaseManager("mockRestServer.db")
+var _httpHandler = services.NewHttpRequestHandler(_dbManager)
 
 func main() {
 	_templates = InitializeTemplates()
 
 	// Routes for REST-API
-	_router.HandleFunc("/api/mock/key/{key}", getMock).Methods("GET")
-	_router.HandleFunc("/api/mock/key/{key}", deleteMock).Methods("DELETE")
-	_router.HandleFunc("/api/mock", createMock).Methods("POST")
-	_router.HandleFunc("/api/mock", updateMock).Methods("PUT")
-	_router.HandleFunc("/api/mock/all", getAllMocks).Methods("GET")
+	_router.HandleFunc("/api/mock/key/{key}", _httpHandler.GetMock).Methods("GET")
+	_router.HandleFunc("/api/mock/key/{key}", _httpHandler.DeleteMock).Methods("DELETE")
+	_router.HandleFunc("/api/mock", _httpHandler.CreateMock).Methods("POST")
+	_router.HandleFunc("/api/mock", _httpHandler.UpdateMock).Methods("PUT")
+	_router.HandleFunc("/api/mock/all", _httpHandler.GetAllMocks).Methods("GET")
 
 	fileServer := http.FileServer(http.Dir("assets/"))
 	_router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
 
-	_router.HandleFunc("/raw/{key}", getMockContent).Methods("GET")
+	_router.HandleFunc("/raw/{key}", _httpHandler.GetMockContent).Methods("GET")
 
 	_router.HandleFunc("/create", func(writer http.ResponseWriter, request *http.Request) {
 		_templates["create"].Execute(writer, nil)
@@ -47,161 +44,6 @@ func main() {
 
 	http.ListenAndServe(":5050", _router)
 	// todo: graceful shutdown? Close DB Connection
-}
-func getMockContent(writer http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	key := vars["key"]
-
-	result, err := _dbManager.GetMock(key)
-
-	if err != nil {
-		log.Fatalf("Error receiving data. %q", err)
-	}
-
-	if result.Id <= 0 {
-		http.NotFound(writer, request)
-	} else {
-		fmt.Fprintf(writer, "%s", result.Content)
-	}
-}
-
-func createMock(writer http.ResponseWriter, request *http.Request) {
-	result, err := ioutil.ReadAll(request.Body)
-
-	var content models.JsonMockPost
-	err = json.Unmarshal(result, &content)
-
-	content.TrimFields()
-
-	if err != nil {
-		writeBadRequest("Unable to unmarshal Json", http.StatusBadRequest, writer)
-		return
-	}
-
-	if len(content.Content) == 0 {
-		writeBadRequest("Content must not be empty.", http.StatusBadRequest, writer)
-		return
-	}
-
-	id, err := _dbManager.SaveMockToDatabase(content.Key, content.Content)
-
-	if err != nil {
-		writeBadRequest(fmt.Sprintf("Error saving data to database. %q", err),
-			http.StatusInternalServerError, writer)
-		return
-	}
-
-	content.Key = id
-	data, err := json.Marshal(content)
-
-	writer.Header().Set("Content-Type", "text/plain")
-	writer.WriteHeader(http.StatusCreated)
-	writer.Write(data)
-}
-
-func getMock(writer http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	key := vars["key"]
-
-	result, err := _dbManager.GetMock(key)
-
-	if err != nil {
-		writeBadRequest(fmt.Sprintf("No mock with key '%s' found", key), http.StatusBadRequest, writer)
-		log.Panicf("Error receiving data. %q", err)
-	}
-
-	if result.Id == 0 {
-		writeBadRequest(fmt.Sprintf("No mock with key '%s' found", key), http.StatusBadRequest, writer)
-		return
-	}
-
-	data, err := json.Marshal(result)
-
-	writer.Header().Set("Content-Type", "text/plain")
-	writer.Write(data)
-}
-
-func getAllMocks(writer http.ResponseWriter, request *http.Request) {
-	result, err := _dbManager.GetAll()
-
-	if err != nil {
-		writeBadRequest(fmt.Sprintf("Error reading all mocks from database."), http.StatusBadRequest, writer)
-		log.Panicf("Error receiving data. %q", err)
-	}
-
-	data, err := json.Marshal(result)
-
-	writer.Header().Set("Content-Type", "text/plain")
-	writer.Write(data)
-}
-
-func updateMock(writer http.ResponseWriter, request *http.Request) {
-	result, err := ioutil.ReadAll(request.Body)
-
-	var content models.JsonMockPost
-	err = json.Unmarshal(result, &content)
-
-	content.TrimFields()
-
-	if err != nil {
-		writeBadRequest("Unable to unmarshal Json", http.StatusBadRequest, writer)
-		return
-	}
-
-	if len(content.Content) == 0 || len(content.Key) == 0 {
-		writeBadRequest("Content and Key must not be empty.", http.StatusBadRequest, writer)
-		return
-	}
-
-	isExisting, err := _dbManager.ContainsKey(content.Key)
-	if isExisting == false {
-		writeBadRequest(fmt.Sprintf("No mock with key '%s' existing.", content.Key), http.StatusBadRequest, writer)
-		return
-	}
-
-	err = _dbManager.UpdateMock(content.Key, content.Content)
-
-	if err != nil {
-		log.Panic("Unable to update item in database")
-	}
-
-	data, err := json.Marshal(content)
-
-	writer.WriteHeader(http.StatusAccepted)
-	writer.Header().Set("Content-Type", "text/plain")
-	writer.Write(data)
-}
-
-func deleteMock(writer http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	key := vars["key"]
-
-	isExisting, err := _dbManager.ContainsKey(key)
-
-	if err != nil {
-		writeBadRequest(fmt.Sprintf("No mock with key '%s' found", key), http.StatusBadRequest, writer)
-		log.Panicf("Error receiving data. %q", err)
-	}
-
-	if isExisting == false {
-		writeBadRequest(fmt.Sprintf("No mock with key '%s' found", key), http.StatusBadRequest, writer)
-		return
-	}
-
-	err = _dbManager.DeleteMock(key)
-	if err != nil {
-		writeBadRequest("Error deleting the mock from database.", http.StatusBadRequest, writer)
-		log.Panicf("Error deleteing mock. %q", err)
-		return
-	}
-
-	writer.WriteHeader(http.StatusAccepted)
-}
-
-func writeBadRequest(errorMessage string, statusCode int, writer http.ResponseWriter) {
-	writer.Header().Set("Content-Type", "text/plain")
-	writer.WriteHeader(statusCode)
-	writer.Write([]byte(errorMessage))
 }
 
 func InitializeTemplates() map[string]*template.Template {
---
+++ services/HttpRequestHandler.go
@@ -0,0 +1,179 @@
+package services
+
+import (
+	"./../models"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type HttpRequestHandler struct {
+	DatabaseManager *DatabaseManager
+}
+
+func NewHttpRequestHandler(dbManager *DatabaseManager) *HttpRequestHandler {
+	instance := &HttpRequestHandler{
+		DatabaseManager: dbManager,
+	}
+
+	return instance
+}
+
+func (handler *HttpRequestHandler) GetMock(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	key := vars["key"]
+
+	result, err := handler.DatabaseManager.GetMock(key)
+
+	if err != nil {
+		writeBadRequest(fmt.Sprintf("No mock with key '%s' found", key), http.StatusBadRequest, writer)
+		log.Panicf("Error receiving data. %q", err)
+	}
+
+	if result.Id == 0 {
+		writeBadRequest(fmt.Sprintf("No mock with key '%s' found", key), http.StatusBadRequest, writer)
+		return
+	}
+
+	data, err := json.Marshal(result)
+
+	writer.Header().Set("Content-Type", "text/plain")
+	writer.Write(data)
+}
+
+func (handler *HttpRequestHandler) DeleteMock(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	key := vars["key"]
+
+	isExisting, err := handler.DatabaseManager.ContainsKey(key)
+
+	if err != nil {
+		writeBadRequest(fmt.Sprintf("No mock with key '%s' found", key), http.StatusBadRequest, writer)
+		log.Panicf("Error receiving data. %q", err)
+	}
+
+	if isExisting == false {
+		writeBadRequest(fmt.Sprintf("No mock with key '%s' found", key), http.StatusBadRequest, writer)
+		return
+	}
+
+	err = handler.DatabaseManager.DeleteMock(key)
+	if err != nil {
+		writeBadRequest("Error deleting the mock from database.", http.StatusBadRequest, writer)
+		log.Panicf("Error deleteing mock. %q", err)
+		return
+	}
+
+	writer.WriteHeader(http.StatusAccepted)
+}
+
+func (handler *HttpRequestHandler) CreateMock(writer http.ResponseWriter, request *http.Request) {
+	result, err := ioutil.ReadAll(request.Body)
+
+	var content models.JsonMockPost
+	err = json.Unmarshal(result, &content)
+
+	content.TrimFields()
+
+	if err != nil {
+		writeBadRequest("Unable to unmarshal Json", http.StatusBadRequest, writer)
+		return
+	}
+
+	if len(content.Content) == 0 {
+		writeBadRequest("Content must not be empty.", http.StatusBadRequest, writer)
+		return
+	}
+
+	id, err := handler.DatabaseManager.SaveMockToDatabase(content.Key, content.Content)
+
+	if err != nil {
+		writeBadRequest(fmt.Sprintf("Error saving data to database. %q", err),
+			http.StatusInternalServerError, writer)
+		return
+	}
+
+	content.Key = id
+	data, err := json.Marshal(content)
+
+	writer.Header().Set("Content-Type", "text/plain")
+	writer.WriteHeader(http.StatusCreated)
+	writer.Write(data)
+}
+
+func (handler *HttpRequestHandler) UpdateMock(writer http.ResponseWriter, request *http.Request) {
+	result, err := ioutil.ReadAll(request.Body)
+
+	var content models.JsonMockPost
+	err = json.Unmarshal(result, &content)
+
+	content.TrimFields()
+
+	if err != nil {
+		writeBadRequest("Unable to unmarshal Json", http.StatusBadRequest, writer)
+		return
+	}
+
+	if len(content.Content) == 0 || len(content.Key) == 0 {
+		writeBadRequest("Content and Key must not be empty.", http.StatusBadRequest, writer)
+		return
+	}
+
+	isExisting, err := handler.DatabaseManager.ContainsKey(content.Key)
+	if isExisting == false {
+		writeBadRequest(fmt.Sprintf("No mock with key '%s' existing.", content.Key), http.StatusBadRequest, writer)
+		return
+	}
+
+	err = handler.DatabaseManager.UpdateMock(content.Key, content.Content)
+
+	if err != nil {
+		log.Panic("Unable to update item in database")
+	}
+
+	data, err := json.Marshal(content)
+
+	writer.WriteHeader(http.StatusAccepted)
+	writer.Header().Set("Content-Type", "text/plain")
+	writer.Write(data)
+}
+
+func (handler *HttpRequestHandler) GetAllMocks(writer http.ResponseWriter, request *http.Request) {
+	result, err := handler.DatabaseManager.GetAll()
+
+	if err != nil {
+		writeBadRequest(fmt.Sprintf("Error reading all mocks from database."), http.StatusBadRequest, writer)
+		log.Panicf("Error receiving data. %q", err)
+	}
+
+	data, err := json.Marshal(result)
+
+	writer.Header().Set("Content-Type", "text/plain")
+	writer.Write(data)
+}
+
+func (handler *HttpRequestHandler) GetMockContent(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	key := vars["key"]
+
+	result, err := handler.DatabaseManager.GetMock(key)
+
+	if err != nil {
+		log.Fatalf("Error receiving data. %q", err)
+	}
+
+	if result.Id <= 0 {
+		http.NotFound(writer, request)
+	} else {
+		fmt.Fprintf(writer, "%s", result.Content)
+	}
+}
+
+func writeBadRequest(errorMessage string, statusCode int, writer http.ResponseWriter) {
+	writer.Header().Set("Content-Type", "text/plain")
+	writer.WriteHeader(statusCode)
+	writer.Write([]byte(errorMessage))
+}
